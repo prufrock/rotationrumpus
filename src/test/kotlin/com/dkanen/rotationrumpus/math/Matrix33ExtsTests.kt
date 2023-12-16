0cfd0ae6605a75dba2340c65a3a82bdbaa7e4800
David Kanenwisher <d.kanen@gmail.com>
2023-12-15 19:34:09 -0600

    Move non-orientation tests to Matrix33

--- src/test/kotlin/com/dkanen/rotationrumpus/math/Matrix33ExtsTests.kt
+++ src/test/kotlin/com/dkanen/rotationrumpus/math/Matrix33ExtsTests.kt
@@ -50,4 +50,90 @@ class Matrix33ExtsTests {
 
         assertEquals(10.0.asRadians, b, 0.0001)
     }
+
+    @Test
+    fun `is not orthogonal`() {
+        // create an orthogonal matrix
+        val o = Matrix33.fromColumnVectors(
+            Vector3(1.0, 2.0, 3.0),
+            Vector3(4.0, 5.0, 6.0),
+            Vector3(7.0, 8.0, 9.0)
+        )
+
+        assertFalse(o.isOrthogonal)
+    }
+
+    @Test
+    fun `is orthogonal`() {
+        val c1 = Vector3(1.0 / 3.0, 2.0 / 3.0, -2.0 / 3.0)
+        val c2 = Vector3(-2.0 / 3.0, 2.0 / 3.0, 1.0 / 3.0)
+        val c3 = Vector3(2.0 / 3.0, 1.0 / 3.0, 2.0 / 3.0)
+
+        // create an orthogonal matrix
+        val o = Matrix33.fromColumnVectors(
+            c1,
+            c2,
+            c3
+        )
+        val ot = o.transposed
+
+        assertEquals(0.0, c1.dot(c2))
+        assertEquals(0.0, c2.dot(c3))
+        assertTrue(ot.isOrthogonal)
+    }
+
+    @Test
+    fun `orthogonalize a matrix nonbiased`() {
+        val c1 = Vector3(1.0 / 3.0, 2.0 / 3.1, -2.0 / 3.0)
+        val c2 = Vector3(-2.0 / 3.0, 2.0 / 3.0, 1.0 / 3.0)
+        val c3 = Vector3(2.0 / 3.0, 1.0 / 3.0, 2.0 / 3.0)
+
+        // create an orthogonal matrix
+        val o = Matrix33.fromColumnVectors(
+            c1,
+            c2,
+            c3
+        )
+        assertFalse(o.isOrthogonal)
+        val t = o * o.transposed
+        val oo = o.orthoganlizeNonbiased(0.25)
+        assertTrue((oo * oo.transposed).eq(Matrix33.IDENTITY, atol = 1e-1))
+    }
+
+    @Test
+    fun `orthogonalize a matrix imu`() {
+        val c1 = Vector3(1.0 / 3.0, 2.0 / 3.1, -2.0 / 3.0)
+        val c2 = Vector3(-2.0 / 3.0, 2.0 / 3.0, 1.0 / 3.0)
+        val c3 = Vector3(2.0 / 3.0, 1.0 / 3.0, 2.0 / 3.0)
+
+        // create an orthogonal matrix
+        val o = Matrix33.fromColumnVectors(
+            c1,
+            c2,
+            c3
+        )
+        assertFalse(o.isOrthogonal)
+        val t = o * o.transposed
+        val oo = o.taylorSeriesNormalization()
+        assertTrue((oo * oo.transposed).eq(Matrix33.IDENTITY, atol = 0.1))
+    }
+
+    @Test
+    fun `orthogonalize a matrix renormalization`() {
+        val c1 = Vector3(1.0 / 3.0, 2.0 / 3.1, -2.0 / 3.0)
+        val c2 = Vector3(-2.0 / 3.0, 2.0 / 3.0, 1.0 / 3.0)
+        val c3 = Vector3(2.0 / 3.0, 1.0 / 3.0, 2.0 / 3.0)
+
+        // create an orthogonal matrix
+        val o = Matrix33.fromColumnVectors(
+            c1,
+            c2,
+            c3
+        )
+        assertFalse(o.isOrthogonal)
+        val t = o * o.transposed
+        var oo = o.renormalization()
+
+        assertTrue((oo * oo.transposed).eq(Matrix33.IDENTITY, atol = 1.0e-3))
+    }
 }
